b11ca54be65c7189cb2f8661886b171113ca861e
enayat789 <enayatbhai2006@gmail.com>
2023-08-06 23:33:01 +0530

    problem no 3 soln

---
+++ problem3.js
@@ -0,0 +1,10 @@
+function printNum() {
+  var num = [];
+  for (let i = 1; i <= 10; i++) {
+    num.push(" 7 " + " x " + i + " = " + i * 7);
+  }
+  return num;
+}
+
+const result = printNum();
+console.log(result);
