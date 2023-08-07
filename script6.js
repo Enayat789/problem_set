5353cc0176bc2350a5c950ea4c55136e90b50bf4
enayat789 <enayatbhai2006@gmail.com>
2023-08-07 08:32:25 +0530

    problem no 6 soln

---
+++ script6.js
@@ -0,0 +1,15 @@
+//  Calculate 10! ( search for factorial.) 
+
+function sumNumbers() {
+    //   var num = [];
+      var sum = 1;
+      for (let i = 1; i <= 10; i++) {
+        sum *= i;
+        // num.push(sum);
+      }
+      return sum;
+    }
+    
+    const result = sumNumbers();
+    console.log(result);
+    
