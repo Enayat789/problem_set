3caabcf236a78954e574176c851d7e486a636fa8
enayat789 <enayatbhai2006@gmail.com>
2023-08-07 18:04:02 +0530

    calculate the sum of numbers from 1 to 10

---
+++ sumofNumbers.js
@@ -0,0 +1,14 @@
+// Calculate the sum of numbers from 1 to 10
+
+function sumNumbers() {
+//   var num = [];
+  var sum = 0;
+  for (let i = 1; i <= 10; i++) {
+    sum += i;
+    // num.push(sum);
+  }
+  return sum;
+}
+
+const result = sumNumbers();
+console.log(result);
