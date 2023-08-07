59a3c21d40a724887aad0fb2b6f99f5e6a726873
enayat789 <enayatbhai2006@gmail.com>
2023-08-07 09:13:22 +0530

    Problem no 9

---
+++ script9.js
@@ -0,0 +1,16 @@
+// Create a function that will convert from Fahrenheit to Celsius
+
+// array=[10,22,322,452,766]
+
+function printNum() {
+  var num = 0;
+  var array = [10, 22, 322, 452, 766];
+  for (let i = 0; i < array.length; i++) {
+    num += array[i];
+    // num.push(i);
+  }
+  return num;
+}
+
+const result = printNum();
+console.log(result);
