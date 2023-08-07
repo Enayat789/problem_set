f3aea39c97327124a51ad59bbe4330a019511e35
enayat789 <enayatbhai2006@gmail.com>
2023-08-07 15:13:23 +0530

    Problem no 13 ka soln

---
+++ script13.js
@@ -0,0 +1,16 @@
+function printNum() {
+  let num1 = 0,
+    num2 = 1,
+    nxTerm;
+
+  for (let i = 0; i < 10; i++) {
+    console.log(num1);
+    nxTerm = num1 + num2;
+    num1 = num2;
+    num2 = nxTerm;
+  }
+  return;
+}
+
+const result = printNum();
+console.log(result);
