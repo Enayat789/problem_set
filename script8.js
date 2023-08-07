55cd4a87b1cbaffeeb97b1d94abe1f93a66f49aa
enayat789 <enayatbhai2006@gmail.com>
2023-08-07 09:01:25 +0530

    problem no 8 soln

---
+++ script8.js
@@ -0,0 +1,19 @@
+// Create a function that will convert from Celsius to Fahrenheit
+
+// function sumNumbers(celsius) {
+//   //   let celsius = 26;
+//   let fahrengeit;
+//   fahrengeit = (celsius * 9) / 5 + 32 + " F ";
+
+//   return fahrengeit;
+// }
+
+function sumNumbers(fahrengeit) {
+  let celsius;
+  celsius = ((fahrengeit - 32) * 5) / 9 + " C ";
+
+  return celsius;
+}
+
+const result = sumNumbers(26);
+console.log(result);
